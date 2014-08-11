4c9d09d0c6a18dee7d680d088c8635fb24de4fad
DonQuality <santosemmanuel@outlook.com>
2014-08-11 10:51:25 -0700

    update html

---
+++ codigo.js
@@ -0,0 +1,20 @@
+  var answer = "puerto rico";
+  var answer2 = "cuba";
+  var answer3 = "asia";
+
+  var Repeat = function(){
+    var guess = prompt("Where do you think i'm from?").toLowerCase();
+    if (guess === answer) {
+    alert('You got that right!!!');
+  } else if (guess === answer2){
+    alert("I am not cuban, but our flag is very similar to their flag, Please try again!!!.");
+    Repeat();
+  } else if (guess === answer3){
+    alert("They call me chino but im not Asian. Try again!!!.");
+    Repeat();
+  } else{
+    alert("Not from there,Try again!!!");
+    Repeat();
+  }
+  };
+  Repeat();
